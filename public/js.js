36ffd5b5212619e94b6c1b44dc21265e15c88f44
AzamHussain7890 <azamforu786@gmail.com>
2022-11-02 10:17:06 -0700

    work complete

---
+++ public/js.js
@@ -0,0 +1,92 @@
+
+
+// prompt IF User value input EXMAPLE
+
+// var PHY = +prompt("Enter you PHY MARKs")
+// var MATHS = +prompt("Enter you MATHS MARKs")
+// var ENGLISH = +prompt("Enter you EGNLISH MARKs")
+// var URDU = +prompt("Enter you URDU MARKs")
+// var CHEMISTRY = +prompt("Enter you CHEMISTRY MARKs")
+// var CALCULAS = +prompt("Enter you CALCULAS MARKs")
+// var DATABASE = +prompt("Enter you BATABSE MARKs")
+
+// var obtainmark = PHY + MATHS + ENGLISH + URDU + CHEMISTRY + CALCULAS + DATABASE;
+// console.log(obtainmark)
+
+// var per = (obtainmark*100)/700;
+// console.log(per,"%")
+
+
+
+
+// If else EXAMPLE # 1
+
+
+
+// var jasir = prompt ("Is Jasir In Home Yes OR Not ")
+// var jasirhome = prompt("Is Jasir Home Lock Not")
+
+
+//  if(jasir == "yes"){
+//     console.log("I Will Give Key Jasir")
+//  }else if (jasirhome == "not lock" ){
+//     console.log("Then I Will Give Bike key Jasir Mother")
+//  }else{
+//     console.log("Come Back Home")
+//  }
+
+
+
+// MARKSHEET EXAM # 1
+
+// var A = +prompt("Enter You Math Marks")
+// var B = +prompt("Enter You Phy Marks")
+// var C = +prompt("Enter You isl Marks")
+// var D = +prompt("Enter You chem Marks")
+// var E = +prompt("Enter You urdu Marks")
+// var f = A + B + C + D + E;
+// console.log(f, "TOTAL MARKS")
+
+// var g = (f * 100) / 500;
+
+// if (g > 90) {
+//     console.log("A+ Grade" +g+ "%")
+// } else if (g > 80) {
+//     console.log("A Grade" +g+ "%")
+// } else if (g > 70) {
+//     console.log("A Grade" +g+ "%")
+// } else if (g > 60) {
+//     console.log("B Grade" +g+ "%")
+// } else if (g > 50) {
+//     console.log("C Grade" +g+ "%")
+// } else {
+//     console.log("FAIL")
+// }
+
+
+// MARKSHEET EXAM # 2
+
+// var A = +prompt("Enter You Math Marks")
+// var B = +prompt("Enter You Phy Marks")
+// var C = +prompt("Enter You isl Marks")
+// var D = +prompt("Enter You chem Marks")
+// var E = +prompt("Enter You urdu Marks")
+// var obtainmark = A + B + C + D + E;
+// console.log(obtainmark, "OBATIN MARK")
+
+// var total = (obtainmark * 100) / 500;
+
+// if (total > 90) {
+//     console.log("A+ Grade" +total+ "%")
+// } else if(total > 80) {
+//     console.log("A Grade" +total+ "%")
+// } else if(total > 70) {
+//     console.log("A Grade" +total+ "%")
+// } else if(total > 60) {
+//     console.log("B Grade" +total+ "%")
+// } else if(total > 50) {
+//     console.log("C Grade" +total+ "%")
+// } else {
+//     console.log("FAIL")
+// }
+
